0f1062fa9fa9f93837188e61e0657a606630a9f8
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-11 12:20:04 +0530

    container

---
+++ lib/Container.ts
@@ -0,0 +1,15 @@
+import axios, { Axios } from 'axios'
+
+export class Container {
+    private readonly _dockerDaemonURL = "http://localhost:2375/v1.24"
+    private readonly _axios!: Axios;
+
+    constructor(){
+        this._axios = axios.create({baseURL: this._dockerDaemonURL})
+    }
+
+
+    async buildImage(file: File){
+        await this._axios.post("/build", file, {headers: {"Content-Type": "application/x-tar"}})
+    }
+}
