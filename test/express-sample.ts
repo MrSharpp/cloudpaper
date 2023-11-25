3f24b1f913aeb18a1230113adb20a18dff11f771
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-25 12:39:02 +0530

    dd

--- test/express-sample.ts
+++ test/express-sample.ts
@@ -1,8 +1,8 @@
-import { DaemonHost } from "../lib/Container";
-import * as tar from "tar";
-import * as fs from "fs";
-import { DockerFileService } from "../lib/DockerFileService";
-import * as path from "path";
+import { DaemonHost } from '../src/lib/Container';
+import * as tar from 'tar';
+import * as fs from 'fs';
+import { DockerFileService } from '../src/lib/DockerFileService';
+import * as path from 'path';
 
 async function run() {
   const host = new DaemonHost();
@@ -11,27 +11,27 @@ async function run() {
   dockerfile.makeDockerFile({
     envs: [],
     port: 3000,
-    repoLink: "https://github.com/lujakob/nestjs-realworld-example-app",
+    repoLink: 'https://github.com/lujakob/nestjs-realworld-example-app',
   });
 
-  console.log(path.join("test", "Dockerfile"));
+  console.log(path.join('test', 'Dockerfile'));
 
-  await dockerfile.exportDockerFile(path.join("test", "Dockerfile"));
+  await dockerfile.exportDockerFile(path.join('test', 'Dockerfile'));
 
-  await tar.c({ file: "./test/docker-tarball.tgz", cwd: "test" }, [
-    "Dockerfile",
+  await tar.c({ file: './test/docker-tarball.tgz', cwd: 'test' }, [
+    'Dockerfile',
   ]);
 
   const resp = await host.buildImage(
-    fs.readFileSync("./test/docker-tarball.tgz"),
-    "userid/express-sample:latest"
+    fs.readFileSync('./test/docker-tarball.tgz'),
+    'userid/express-sample:latest',
   );
   console.log(resp);
 
   const respCont = await host.createContainer(
-    "aa",
-    "userid/express-sample",
-    3000
+    'aa',
+    'userid/express-sample',
+    3000,
   );
 
   const respRun = await host.runContainer(respCont.Id);
