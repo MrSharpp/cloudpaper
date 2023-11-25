20f62cde740b0c18036290da18f9be5554cbff59
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-25 12:50:10 +0530

    Update build.controller.ts

--- src/build/build.controller.ts
+++ src/build/build.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BuildService } from './build.service';
 
 @Controller()
-export class BuildController {}
+export class BuildController {
+  constructor(private readonly buildService: BuildService) {}
+
+  @Post('build')
+  buildUserSourceCode(@Body() body) {}
+}
