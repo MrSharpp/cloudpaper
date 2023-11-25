5dbd955726b57cab5bd19235436cd902ac52beee
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-25 12:58:59 +0530

    Update build.service.ts

--- src/build/build.service.ts
+++ src/build/build.service.ts
@@ -6,6 +6,15 @@ import { paths } from 'src/constants';
 export class BuildService {
   cloneRepoToTmpPath(repoUrl: string) {
     // Right now it only supports public git repositories
-    spawn(`cd ${paths.tmpPath}; git clone ${repoUrl}`);
+    const cp = spawn(`cd ${paths.tmpPath}; git clone ${repoUrl}`);
+
+    return new Promise<void>((res, rej) => {
+      cp.on('exit', (code) => {
+        if (code == 0) res();
+        else rej();
+      });
+
+      cp.on('error', (err) => console.log(err.message));
+    });
   }
 }
